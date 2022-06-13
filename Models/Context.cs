df2300f5d8d27cf15e6138cce6ae34e473c44c91
Детелинка Венциславова Трифонова <stu2001261021@uni-plovdiv.bg>
2022-06-13 22:19:27 +0300

    commit

---
+++ Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace A_Work_5_API.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; } = null!;
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace A_Work_5_API.Models
+{
+    public class Item
+    {
+        public long Id { get; set; }
+
+        public string? CurrencyMark { get; set; }
+
+        public double Currency { get; set; }
+
+        public byte TimePeriod { get; set; }
+
+        public double FinalIncome { get => Currency; }
+    }
+}
